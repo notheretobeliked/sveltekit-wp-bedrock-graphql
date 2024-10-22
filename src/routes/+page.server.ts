9e7fcc65c4bacf6cacdd30d390b302ce8bcf5762
Erik Hartin <erik@nhtbl.studio>
2024-10-23 00:17:26 +0100

    hotfix: do not prerender homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-export const prerender = true
+export const prerender = false
 
 import PageContent from '$lib/graphql/query/page.graphql?raw'
 import { checkResponse, graphqlQuery } from '$lib/utilities/graphql'
