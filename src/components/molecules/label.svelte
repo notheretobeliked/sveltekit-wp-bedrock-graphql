be094330b715cc8a01d7cc1c621f16707b8662a5
Erik Hartin <erik@nhtbl.studio>
2024-11-04 13:27:31 +0000

    Fix: Underlines when mobile

--- src/components/molecules/label.svelte
+++ src/components/molecules/label.svelte
@@ -135,7 +135,7 @@
 						<LabelAndTitle
 							{label}
 							{title}
-							underline={smallScreen ? (index < labelData.group1.length - 1) : shouldUnderline(index, labelData.group1.length)}
+							underline={smallScreen ? true : shouldUnderline(index, labelData.group1.length)}
 							/>
 					{/each}
 				</div>
@@ -158,7 +158,7 @@
 						<LabelAndTitle
 							{label}
 							{title}
-							underline={smallScreen ? (index < labelData.group1.length - 1) : shouldUnderline(index, labelData.group1.length)}
+							underline={smallScreen ? true : shouldUnderline(index, labelData.group1.length)}
 
 						/>
 					{/each}
@@ -170,7 +170,7 @@
 						<LabelAndTitle
 							{label}
 							{title}
-							underline={smallScreen ? (index < labelData.group1.length - 1) : shouldUnderline(index, labelData.group1.length)}
+							underline={smallScreen ? true : shouldUnderline(index, labelData.group1.length)}
 
 						/>
 					{/each}
