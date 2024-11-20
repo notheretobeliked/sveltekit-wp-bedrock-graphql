7ce39e7b2e2cab2139b8d381572da0d8c0b9b0a8
Erik Hartin <erik@nhtbl.studio>
2024-11-20 14:13:05 +0000

    revert prerender on library

--- src/routes/[lang]/library/+page.server.ts
+++ src/routes/[lang]/library/+page.server.ts
@@ -1,4 +1,4 @@
-export const prerender = true
+export const prerender = false
 import { error } from '@sveltejs/kit'
 import { restructureLibraryItems, extractArtists, extractPublishers, extractAuthors, getYearRange } from '$lib/server/utilities'
 import type { PageServerLoad } from './$types'
