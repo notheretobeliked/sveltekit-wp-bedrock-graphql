2e3d0f833ed3d2655d3daa3ddb5b0af8d1b66337
Erik Hartin <erik@nhtbl.studio>
2024-12-03 11:47:48 +0000

    Feat: Allow Read More button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -9,10 +9,16 @@
 	export let colourClass: string = 'bg-white' // Assuming this is a default style
 	export let textColourClass: string = 'text-black' // Assuming this is a default style
 	if (active) colourClass = 'bg-black text-white-pure'
+	function handleClick(event: MouseEvent) {
+		if (url === '#') {
+			event.preventDefault()
+		}
+	}
 </script>
 
 <a
 	href={url}
+	on:click={handleClick}
 	class="{colourClass} rounded-lg border-black transition-all duration-500 hover:bg-black hover:text-white-pure hover:border-white py-2 px-4 border-white border {textClass} {textColourClass} font-martina cursor-pointer"
 	role="button"
 >
--- src/routes/[lang]/+page.svelte
+++ src/routes/[lang]/+page.svelte
@@ -2,13 +2,32 @@
 	import type { EditorBlock } from '$lib/graphql/generated'
 	import { onMount } from 'svelte';
 	import BlockRenderer from '$components/BlockRenderer.svelte'
+	import Button from '$components/Button.svelte'
 	import type { PageData } from './$types'
+	
 	export let data: PageData
 	let editorBlocks: EditorBlock[], uri: string
-	console.log(data)
+	let showFullContent = false
+	let visibleBlocks
+	let hiddenBlocks
+
 	$: {
 		;({ editorBlocks, uri } = data as unknown as { editorBlocks: EditorBlock[], uri: string })
+		console.log(editorBlocks)
 	}
+
+	// Split blocks at read-more block
+	$: {
+		const readMoreIndex = editorBlocks?.findIndex(block => block.name === 'core/read-more')
+		visibleBlocks = readMoreIndex === -1 
+			? editorBlocks 
+			: editorBlocks.slice(0, readMoreIndex)
+		
+		hiddenBlocks = readMoreIndex === -1
+			? [] 
+			: editorBlocks.slice(readMoreIndex + 1)
+	}
+
 	let lang = data.languageCode as 'ar' | 'en'
 	let headerHeight: number;
 
@@ -19,11 +38,33 @@
 		}
 	});
 
+	function handleReadMore() {
+		showFullContent = !showFullContent
+	}
 </script>
 
-
 <main class="min-h-screen {lang === 'ar' ? 'main-ar' : ''}" style="padding-top: {headerHeight}px;">
-	{#each editorBlocks as block, index (block.clientId)}
+	{#each visibleBlocks as block (block.clientId)}
 		<BlockRenderer {block} />
 	{/each}
+
+	{#if hiddenBlocks.length > 0}
+		<div class="flex justify-center mt-8">
+			<div on:click={handleReadMore}>
+				<Button 
+					label={showFullContent ? 'Show Less' : 'Read More'} 
+					active={showFullContent}
+					url="#"
+				/>
+			</div>
+		</div>
+
+		{#if showFullContent}
+			<div transition:slide>
+				{#each hiddenBlocks as block (block.clientId)}
+					<BlockRenderer {block} />
+				{/each}
+			</div>
+		{/if}
+	{/if}
 </main>
