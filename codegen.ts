a7bd8ad8c33d9388a64db4805c4b3b93d3ab6520
Erik Hartin <erik@nhtbl.studio>
2026-02-12 13:24:36 +0000

    Fix: update codegen

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+	schema: process.env.GRAPHQL_ENDPOINT,
+	documents: ['./src/**/*.graphql', '!./src/**/*preview*.graphql'],
+	generates: {
+		'./src/lib/graphql/generated.ts': {
+			plugins: ['typescript', 'typescript-operations', 'typescript-generic-sdk']
+		}
+	}
+}
+
+export default config
